e81eec304eb7dbc5ac7227a46c919c922aca1e06
Summer Greene <summer@1011i.com>
2015-09-15 08:51:30 -0700

    Subnav Safari fix

--- sites/all/themes/headquarters/js/init.js
+++ sites/all/themes/headquarters/js/init.js
@@ -2,11 +2,11 @@ jQuery(document).ready(function($){
 
     // External Links and PDF's Open in New Window
 
-    $('a[href^="https://"], a[href^="http://"], a[href$=".pdf"').not('.view-calendar a[href^="https://"], .view-calendar a[href^="http://"]').attr('target', '_blank');
+    $('a[href^="https://"], a[href^="http://"], a[href$=".pdf"]').not('.view-calendar a[href^="https://"], .view-calendar a[href^="http://"]').attr('target', '_blank');
 
     // Magnific Popup
 
-    $('a[href$=".pdf"').magnificPopup({
+    $('a[href$=".pdf"]').magnificPopup({
         disableOn: 700,
         type: 'iframe',
         mainClass: 'mfp-fade',
