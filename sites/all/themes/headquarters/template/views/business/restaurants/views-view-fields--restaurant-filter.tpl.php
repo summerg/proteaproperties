e7342e429a9911e0a12e7df9025ef08d4d50d248
Summer Greene <summer@1011i.com>
2017-06-23 12:45:59 -0700

    slider fix, analytics attribute on restaurant/shop filters

--- sites/all/themes/headquarters/template/views/business/restaurants/views-view-fields--restaurant-filter.tpl.php
+++ sites/all/themes/headquarters/template/views/business/restaurants/views-view-fields--restaurant-filter.tpl.php
@@ -1,5 +1,5 @@
 <li>
-	<a class="btn btn-outline" href="#" data-filter=".<?php print strtolower(preg_replace('/\s/', '', strip_tags($fields['name']->content))); ?>">
+	<a class="btn btn-outline" href="#" data-filter=".<?php print strtolower(preg_replace('/\s/', '', strip_tags($fields['name']->content))); ?>" onClick="ga('send', 'filter', 'restaurants', '<?php print strip_tags($fields['name']->content); ?>');">
 		<?php print $fields['name']->content; ?>
 	</a>
 </li>
